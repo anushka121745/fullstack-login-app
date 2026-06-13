5c1c4a445b017f5ad857e17af4f4515faf896ee6
anushka121745 <anushka.121745@gmail.com>
2026-06-13 15:57:42 +0530

    Add SSL support for cloud database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_HOST === "localhost" ? false : { rejectUnauthorized: false },
 });
 
 module.exports = pool;
